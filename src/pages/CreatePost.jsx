b33de2801606357090fba27c9d1ec0df1460a2c4
Rafey <rafeldo36@gmail.com>
2024-06-01 17:35:30 +0530

    first commit

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -49,7 +49,7 @@ const CreatePost = ({ contract, account, provider }) => {
     if (form.prompt || speech) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://dall-server.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -81,7 +81,7 @@ const CreatePost = ({ contract, account, provider }) => {
     if (form.prompt || speech && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://dall-server.onrender.com/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
